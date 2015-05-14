e6cc8a85d1f95c222a7f4215bcad531670cf193a
wilsto <willy.stophe.pro@gmail.com>
2015-05-14 20:38:02 +0200

    0.3.9
    Mise en place d'un ping via newrelic

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+var newrelic = require('newrelic');
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
@@ -15,22 +16,24 @@ var config = require('./config/environment');
 mongoose.connect(config.mongo.uri, config.mongo.options);
 
 // Populate DB with sample data
-if(config.seedDB) { require('./config/seed'); }
+if (config.seedDB) {
+    require('./config/seed');
+}
 
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
 var socketio = require('socket.io')(server, {
-  serveClient: (config.env === 'production') ? false : true,
-  path: '/socket.io-client'
+    serveClient: (config.env === 'production') ? false : true,
+    path: '/socket.io-client'
 });
 require('./config/socketio')(socketio);
 require('./config/express')(app);
 require('./routes')(app);
 
 // Start server
-server.listen(config.port, config.ip, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+server.listen(config.port, config.ip, function() {
+    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
 
 // Expose app
