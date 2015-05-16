10795d1e72694132013012b0b97b5f1d8f66bede
wilsto <willy.stophe.pro@gmail.com>
2015-05-16 17:59:55 +0200

    0.3.11
    Mise à disposition de l'API pour la mobile app

--- server/config/express.js
+++ server/config/express.js
@@ -20,40 +20,51 @@ var mongoStore = require('connect-mongo')(session);
 var mongoose = require('mongoose');
 
 module.exports = function(app) {
-  var env = app.get('env');
+    var env = app.get('env');
 
-  app.set('views', config.root + '/server/views');
-  app.engine('html', require('ejs').renderFile);
-  app.set('view engine', 'html');
-  app.use(compression());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
-  app.use(methodOverride());
-  app.use(cookieParser());
-  app.use(passport.initialize());
+    var allowCrossDomain = function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        next();
+    };
 
-  // Persist sessions with mongoStore
-  // We need to enable sessions for passport twitter because its an oauth 1.0 strategy
-  app.use(session({
-    secret: config.secrets.session,
-    resave: true,
-    saveUninitialized: true,
-    store: new mongoStore({ mongoose_connection: mongoose.connection })
-  }));
-  
-  if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, 'public')));
-    app.set('appPath', config.root + '/public');
-    app.use(morgan('dev'));
-  }
+    app.set('views', config.root + '/server/views');
+    app.engine('html', require('ejs').renderFile);
+    app.set('view engine', 'html');
+    app.use(compression());
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+    app.use(allowCrossDomain);
+    app.use(cookieParser());
+    app.use(passport.initialize());
 
-  if ('development' === env || 'test' === env) {
-    app.use(require('connect-livereload')());
-    app.use(express.static(path.join(config.root, '.tmp')));
-    app.use(express.static(path.join(config.root, 'client')));
-    app.set('appPath', 'client');
-    app.use(morgan('dev'));
-    app.use(errorHandler()); // Error handler - has to be last
-  }
+    // Persist sessions with mongoStore
+    // We need to enable sessions for passport twitter because its an oauth 1.0 strategy
+    app.use(session({
+        secret: config.secrets.session,
+        resave: true,
+        saveUninitialized: true,
+        store: new mongoStore({
+            mongoose_connection: mongoose.connection
+        })
+    }));
+
+    if ('production' === env) {
+        app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+        app.use(express.static(path.join(config.root, 'public')));
+        app.set('appPath', config.root + '/public');
+        app.use(morgan('dev'));
+    }
+
+    if ('development' === env || 'test' === env) {
+        app.use(require('connect-livereload')());
+        app.use(express.static(path.join(config.root, '.tmp')));
+        app.use(express.static(path.join(config.root, 'client')));
+        app.set('appPath', 'client');
+        app.use(morgan('dev'));
+        app.use(errorHandler()); // Error handler - has to be last
+    }
 };
